08f4cec3f758067425aec83dc39a54c780f24152
giga487 <giga487@hotmail.com>
2018-11-21 18:01:00 +0100

    Jacobiano

--- main.m
+++ main.m
@@ -5,23 +5,39 @@ close all;
 clc;
 
 addpath('utils');
+%% Esempio RR planare 2. Scemissimo.
+syms x1 x2 x3 L1 L2 L3
 
-%%Manipolatore Antroporfo del libro Robotica, modellistica, pianificazione
-%%e controllo.
+A01 = [Rz_rad(x1),[L1*cos(x1);L1*sin(x1);0];
+      0,0,0, 1];
 
-L1 = 1;
-L2 = L1;
-L3 = L2;
-syms a1 a2 a3 
+A12 = [Rz_rad(x2),[L2*cos(x2);L2*sin(x2);0];
+      0,0,0, 1];
+  
+T02 = A01*A12;
 
-T = [c(a1)*c(a2+a3), -c(a1)*s(a2+a3), s(a1), c(a1)*(L2*c(a2)+L3*c(a2+a3));
-     s(a1)*c(a2+a3), -s(a1)*s(a2+a3), -c(a1), s(a1)*(L2*c(a2)+L3*c(a2+a3));
-     s(a2+a3), c(a2+a3), 0 , L2*s(a2)+L3*s(a2+a3);
-     0,0,0,1];
+x = T02(1,4);
+y = T02(2,4);
 
-%% Calcolo del Jacobiano
+J1 = jacobian(x,[x1,x2])
+J2 = jacobian(y,[x1,x2])
 
-J = jacobian(T,[a1,a2,a3]);
+J = [J1;J2]
+
+%% SLIDE 38 di 101 05 Gabiccini
+
+syms theta psi phi
+
+A01 = Rz_rad(theta);
+A02 = Rx_rad(psi);
+A03 = Rz_rad(phi);
+
+T = A01*A02*A03
+
+J1 =  jacobian(T(1,:),[theta,psi,phi])
+J2 =  jacobian(T(2,:),[theta,psi,phi])
+
+
 
 
 
