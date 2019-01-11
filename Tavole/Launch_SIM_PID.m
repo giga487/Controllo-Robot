d0e9b96f42ab8c3b65688475e8656d57f530ce04
unknown <amato.pierluigi@gmail.com>
2019-01-11 15:20:40 +0100

    Automatizzazione, da aggiungere l'errore

--- Tavole/Launch_SIM_PID.m
+++ Tavole/Launch_SIM_PID.m
@@ -7,19 +7,15 @@ Kp = -50;
 Kd = -200;
 setpoint = 0;
 
-parameter_true(1) = 0.25;
-parameter_true(2) = 0.05;
-parameter_true(3) = 0.40;
-parameter_true(4) = 0.30;
-parameter_true(5) = 0.12;
+parameter_true(1) = 0.45; %Lunghezza asta
+parameter_true(2) = 0.05; %ASTA
+parameter_true(3) = 0.40; %m ruota
+parameter_true(4) = 0.30; %m motore
+parameter_true(5) = 0.12; %diametro
 
 noise1 = 0.1;
 
-parameter_estimated(1) = 0.25 + noise1;
-parameter_estimated(2) = 0.05 + noise1; %ASTA
-parameter_estimated(3) = 0.40 + noise1; %m ruota
-parameter_estimated(4) = 0.30 + noise1; %m motore
-parameter_estimated(5) = 0.12 + noise1; %diametro
+parameter_estimated = parameter_true + noise1;
 
 
 sim('PD',15)
