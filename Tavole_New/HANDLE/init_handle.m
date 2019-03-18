09875f87a6438a1ebed8c5221abb638aae3ed3e2
Pierluigi Amato <amato.pierluigi@gmail.com>
2019-03-18 14:56:33 +0100

    Portatile ho fatto cose, bo

---
+++ Tavole_New/HANDLE/init_handle.m
@@ -0,0 +1,11 @@
+%some parameters: mass, lenght of joint
+
+%coordinate of center.
+
+x_L = -5;
+x_R = 5;
+y_D = -5;
+y_U = 5;
+
+x0 = 1;
+y0 = 1;
---
+++ Tavole_New/HANDLE/main.m
@@ -0,0 +1,9 @@
+%File di lancio del robot e di tutti i controlli.
+
+clc
+clear
+close all
+
+addpath('utils');
+
+init_handle
