c228a13769aa36be7f2d644d6d2d05cd81648017
giga487 <giga487@hotmail.com>
2019-04-30 19:12:02 +0200

    Ok

--- Tavole_NEW/InvertePendulumMobile/Tavola3.m
+++ Tavole_NEW/InvertePendulumMobile/Tavola3.m
@@ -9,47 +9,117 @@ g1 = sym('g1', [6 1], 'real'); % state
 g_1 = [0;0;0;0;0;0;0];
 g_2 = [0;0;0;0;0;0;1];
 
-syms H D cz Mb R g Iwa Mw Ka Iyy Ixx Izz G v real
+% syms b H D cz Mb R g Iwa Mw Ka Iyy Ixx Izz G v1 v2 u1 u2 real
+Mb = 35;
+Mw = 5;
+R = 0.25;
+cz = R;
+b = 0.2;
+Ixx = 2.10;
+Iyy = 1.82;
+Izz = 0.64;
+Iwa = 0.15;
+Iwd = 0.08;
+g = 9.81;
+H = 0.5*Mb*(R^2)*Izz+Iwa*Izz-Mw*R^2*Ixx-Iwa*Ixx-Mb*cz^2*Mw*R^2-Mb*cz^2*Iwa-0.5*Mb*R^2*Ixx+Mw*R^2*Izz;
+Ka = (-4*Iyy*Mb*R^2*cz-3*R^2*Mb^2*cz^3+Mb*R^2*cz*(Ixx-Izz))*sin(x(4))+(Mb*R^2*cz*(Ixx-Izz)+R^2*Mb^2*cz^3)*sin(3*x(4));
+D = (Mb*cos(x(4))*cz*R)^2+((-Mb^2-2*Mw*Mb)*cz^2-2*Iyy*Mw-Iyy*Mb)*R^2-2*Mb*cz^2*Iwa-2*Iyy*Iwa;
+G = (-Mb*cz^2+Izz-Ixx)*(R*cos(x(4)))^2+(Mb*cz^2+Ixx+2*Iwd+2*b^2*Mw)*R^2+2*b^2*Iwa;
+syms v1 v2
+
 f2(1) = sin(2*x(4))*((x(7))^2)*H/D+ (1/2)*(Mb*cz*R*x(5))^2*sin(2*x(4))/D+(1/2)*(-(Mb*R)^2 *cz-4*Iwa*Mb*cz-4*Mb*Mw*cz*R^2)*g*sin(x(4))/D;
 f2(2) = Ka*(x(7))^2 + (1/2)*g*sin(2*x(4))/D*(Mb*cz*R)^2 + (1/4)*(-4*Iyy*Mb*R^2*cz-4*R^2*Mb^2*cz^3)*sin(2*x(4))*x(5)^2/D;
 f2(3) = (-(Ixx-Izz)*R^2-Mb*cz^2*R^2)*sin(2*x(4))*x(5)*x(7)/G - sin(x(4))*R^2*Mb*cz*x(6)*x(7)/G;
 
-A = Mb*R^2 + 2*Mw*R^2 + 2*Iwa + Mb*cos(x(4))*cz*R;
-B = -R*(Mb*cos(x(4))*cz*R + Iyy + Mb*cz^2);
+g15 = (Mb*R^2 + 2*Mw*R^2 + 2*Iwa + Mb*cos(x(4))*cz*R)/D;
+g16 = -R*(Mb*cos(x(4))*cz*R + Iyy + Mb*cz^2)/D;
+g17 = R*b/G;
 
-g_1 = [0;0;0;0;A;B;0];
+g25 = g15;
+g26 = g16;
+g27 = -g17;
 
-f = [x(6)*cos(x(3));
+g_1 = [0;0;0;0;g15;g16;g17];
+g_2 = [0;0;0;0;g25;g26;g27];
+
+%Cambio ingressi
+
+% v1 = (u1+u2)/D;
+% v2 = (u1-u2)*R*b/G;
+
+%Ottengo quindi
+g1_hat = [0;0;0;0;(Mb*R^2 + 2*Mw*R^2 + 2*Iwa + Mb*cos(x(4))*cz*R);-R*(Mb*cos(x(4))*cz*R + Iyy + Mb*cz^2);0];
+g2_hat = [0;0;0;0;0;0;1];
+
+f = vpa([x(6)*cos(x(3));
      x(6)*sin(x(3));
      x(7);
      x(5);
-     f2(:)];   
+     f2(:)],2);   
  
 y = [x(3);x(4)];
- 
+
+f_tot = vpa(f + g1_hat*v1 + g2_hat*v2,2)
+
+%l'equilibrio lo pongo in [x,y,tetha,0,0,0,0]
+
+%% Studio
+syms f_fun(x1,x2,x3,x4,x5,x6,x7) g1_fun(x1,x2,x3,x4,x5,x6,x7) g2_fun(x1,x2,x3,x4,x5,x6,x7)
+
+f_fun(x1,x2,x3,x4,x5,x6,x7) = f;
+g1_fun(x1,x2,x3,x4,x5,x6,x7) = g1_hat*v1;
+g2_fun(x1,x2,x3,x4,x5,x6,x7) = g2_hat*v2;
+% 
+% syms x1 x2 x3 x4 x5 x6 x7 real
+syms x1 x2 x3 
+x4 = 0;
+x5 = 0;
+x6 = 0;
+x7 = 0;
+
+f_now = f_fun(x1,x2,x3,x4,x5,x6,x7);
+g1_now = vpa(g1_fun(x1,x2,x3,x4,x5,x6,x7),2);
+g2_now = g2_fun(x1,x2,x3,x4,x5,x6,x7);
+
+% Il sistema non è controllabile attorno ad il suo punto di equilibrio ma è
+% Small time locally controllable e strongly accessible
+
+
 %% Gestione feedback linearization
 
-Lf_h1 = jacobian(y(1),x)*f;
-Lf_h2 = jacobian(y(2),x)*f;
+syms f_fun(x1,x2,x3,x4,x5,x6,x7) g1_fun(x1,x2,x3,x4,x5,x6,x7) g2_fun(x1,x2,x3,x4,x5,x6,x7)
 
-Lf_Lf_h1 = jacobian(Lf_h1,x)*f;
-Lf_Lf_h2 = jacobian(Lf_h2,x)*f;
+f_fun(x1,x2,x3,x4,x5,x6,x7) = f;
 
-Lg1_Lf_h1 = jacobian(Lf_h1,x)*g_1;
-Lg1_Lf_h2 = jacobian(Lf_h2,x)*g_1;
+syms x1 x2 x3 x4 x5 x6 x7 real
 
-Lg2_Lf_h1 = jacobian(Lf_h1,x)*g_2;
-Lg2_Lf_h2 = jacobian(Lf_h2,x)*g_2;
+f_now = vpa(f_fun(x1,x2,x3,x4,x5,x6,x7),2);
+% D0 = [g_1,g_2];
+% D = [f,g_1,g_2];
 
-y_01 = y(1);
-y_11 = Lf_h1;
-y_21 = Lf_Lf_h1 + Lg1_Lf_h1 + Lg2_Lf_h1;
 
-y_02 = y(1);
-y_12 = Lf_h2;
-y_22 = Lf_Lf_h2 + Lg1_Lf_h2 + Lg2_Lf_h2;
+y1 = y(1);
+dy1 = jacobian(y1(1),x)*f;
+L2f_h1 = jacobian(dy1,x)*f;
+Lg1_Lf_h1 = jacobian(dy1,x)*g1_hat*v1;
+Lg2_Lf_h1 = jacobian(dy1,x)*g2_hat*v2;
+ddy1 = vpa(L2f_h1 + Lg1_Lf_h1 + Lg2_Lf_h1,2);
 
-T = [Lf_Lf_h1;Lf_Lf_h2];
-E = [Lg1_Lf_h1,Lg2_Lf_h1;Lg1_Lf_h2,Lg2_Lf_h2];
+y2 = y(2);
+dy2 = jacobian(y2,x)*f;
+L2f_h2 = jacobian(dy2,x)*f;
+Lg1_Lf_h2 = jacobian(dy2,x)*g1_hat*v1;
+Lg2_Lf_h2 = jacobian(dy2,x)*g2_hat*v2;
+ddy2 = vpa(L2f_h2 + Lg1_Lf_h2 + Lg2_Lf_h2,2);
 
-INPUT = -inv(E)*(T-v);
+
+
+
+
+
+
+
+
+
+
+
